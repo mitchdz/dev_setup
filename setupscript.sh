121413a6a5c5b8a9d4ae72d09dd379efc8d30e5e
lightdz <mitch_dz@hotmail.com>
2018-04-04 09:04:18 -0700

    added moving the circlelock image.

--- setupscript.sh
+++ setupscript.sh
@@ -41,4 +41,4 @@ cp i3blocks.conf 	$CONFIG_PATH
 cp background.jpg 	$CONFIG_PATH
 cp config 		$CONFIG_PATH
 cp i3lock-transparent	$CONFIG_PATH
-
+cp circlelock.png         /home/$USER/Pictures/circlelock.png
