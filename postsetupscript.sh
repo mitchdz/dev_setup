ea787c84abb8639704bf365efe240f8c18db639d
lightdz <mitch_dz@hotmail.com>
2018-04-12 19:27:34 -0700

    added setupscript

---
+++ postsetupscript.sh
@@ -0,0 +1,11 @@
+CONFIG_PATH=~/.config/i3/
+
+
+cd ~/git/desktop_i3_setup/
+
+
+cp i3blocks.conf        $CONFIG_PATH
+cp background.jpg       $CONFIG_PATH
+cp config               $CONFIG_PATH
+cp i3lock-transparent   $CONFIG_PATH
+cp circlelock.png       /home/$USER/Pictures/circlelock.png
--- setupscript.sh
+++ setupscript.sh
@@ -7,18 +7,20 @@ sudo add-apt-repository ppa:aguignard/ppa
 sudo apt-get update
 sudo apt-get install sudo apt-get install libxcb-xrm-dev
 
-sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev
+sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev -y
 #--------------------------------------------------------#
 #-----------------Personal Dependencies------------------#
-sudo apt-get install scrot		#commandline tool to take images
-sudo apt-get install ImageMagick	#commandline tool to edit images
-sudo apt-get install compton		#X11 compositor
-sudo apt-get install i3blocks		#i3 program for the bar.
-sudo apt-get install feh		#program to view files for my i3lock
-sudo apt-get install rofi		#program to select files
-sudo apt-get install i3lock		#i3 lock screen.
-sudo apt-get install i3status		#i3 program to run in i3block
-sudo apt-get install thunar		#file manager I use.
+sudo apt-get install scrot ImageMagick compton i3blocks feh rofi i3lock i3status thunar terminator -y
+#sudo apt-get install scrot		#commandline tool to take images
+#sudo apt-get install ImageMagick	#commandline tool to edit images
+#sudo apt-get install compton		#X11 compositor
+#sudo apt-get install i3blocks		#i3 program for the bar.
+#sudo apt-get install feh		#program to view files for my i3lock
+#sudo apt-get install rofi		#program to select files
+#sudo apt-get install i3lock		#i3 lock screen.
+#sudo apt-get install i3status		#i3 program to run in i3block
+#sudo apt-get install thunar		#file manager I use.
+#sudo apt-get install terminator	#terminal of choice.
 #-----------------Installing and making i3---------------#
 mkdir ~/development/
 cd ~/development/
@@ -44,8 +46,10 @@ cd ~/git/desktop_i3_setup/
 
 ## TODO: reboot and then move config files.
 
-cp i3blocks.conf 	$CONFIG_PATH
-cp background.jpg 	$CONFIG_PATH
-cp config 		$CONFIG_PATH
-cp i3lock-transparent	$CONFIG_PATH
-cp circlelock.png       /home/$USER/Pictures/circlelock.png
+reboot
+
+#cp i3blocks.conf 	$CONFIG_PATH
+#cp background.jpg 	$CONFIG_PATH
+#cp config 		$CONFIG_PATH
+#cp i3lock-transparent	$CONFIG_PATH
+#cp circlelock.png       /home/$USER/Pictures/circlelock.png
