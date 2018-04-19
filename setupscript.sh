9b80593697414734a24f21babdb40cb7fd4582f2
lightdz <mitch_dz@hotmail.com>
2018-04-18 17:28:50 -0700

    added -y for ppa's

--- setupscript.sh
+++ setupscript.sh
@@ -14,8 +14,8 @@ LOGINUSER=$(whoami | awk '{print $1}')	#this is incase someone runs as root.
 CONFIG_PATH=/home/$LOGINUSER/.config/i3/
 
 #-------------------i3-gaps Dependencies----------------#
-sudo add-apt-repository ppa:aguignard/ppa
-sudo add-apt-repository ppa:dawidd0811/neofetch
+sudo add-apt-repository ppa:aguignard/ppa -y
+sudo add-apt-repository ppa:dawidd0811/neofetch -y
 sudo apt-get update
 sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev -y
 #--------------------------------------------------------#
